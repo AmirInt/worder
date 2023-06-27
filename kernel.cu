602056e505067c54a3d65a2123130a20bc488056
AmirInt <amir.intelli@gmail.com>
2023-06-27 17:24:16 +0100

    Add data

--- kernel.cu
+++ kernel.cu
@@ -1,8 +1,7 @@
-﻿
+﻿#include <windows.h>
+#include <iostream>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
-#include <stdio.h>
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
