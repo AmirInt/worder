444944f2e3522202d51795e07e9a650e64e8f56d
AmirInt <amir.intelli@gmail.com>
2023-06-27 22:45:31 +0100

    (kernels.cuh ; kernels.cu ; main.cu ; kernel_calls.cuh ; kernel_calls.cu): Seperate kernel and kernel source codes. && kernel.cu: Rename to main.cu.

---
+++ kernel_calls.cuh
@@ -0,0 +1,9 @@
+#ifndef WORDER_KERNEL_CALLS_CUH_
+#define WORDER_KERNEL_CALLSS_CUH_
+
+namespace kernel_calls
+{
+
+}
+
+#endif // WORDER_KERNEL_CALLSS_CUH_
---
+++ kernels.cuh
@@ -0,0 +1,9 @@
+#ifndef WORDER_KERNELS_CUH_
+#define WORDER_KERNELS_CUH_
+
+namespace kernels
+{
+
+}
+
+#endif // WORDER_KERNELS_CUH_
