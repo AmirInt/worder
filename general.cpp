3c68a768d008875c4f43282172968e23fbd18ffb
AmirInt <amir.intelli@gmail.com>
2023-06-27 23:09:13 +0100

    general.hpp:general::(readKeywordFile ; readDataFile): Declare functions.

---
+++ general.cpp
@@ -0,0 +1,14 @@
+#include "general.hpp"
+
+namespace general
+{
+	void readKeywordFile(const char* file_path, char** keywords, size_t size)
+	{
+
+	}
+
+	void readDataFile(const char* file_path, char** keywords, size_t size)
+	{
+
+	}
+}
--- general.hpp
+++ general.hpp
@@ -1,9 +1,13 @@
 #ifndef WORDER_GENERAL_HPP_
 #define WORDER_GENERAL_HPP_
 
+// C++
+
 namespace general
 {
+	void readKeywordFile(const char* file_path, char** keywords, size_t size);
 
+	void readDataFile(const char* file_path, char** words, size_t size);
 }
 
 #endif // WORDER_GENERAL_HPP_
