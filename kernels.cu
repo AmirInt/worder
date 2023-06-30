cdc56953df948f7bffa686e5af322c92a118dbc2
AmirInt <amir.intelli@gmail.com>
2023-06-30 19:49:15 +0100

    kernels.cu:kernels::removeExcessives: Implement function to remove excessive punctuation marks and clear data.

--- kernels.cu
+++ kernels.cu
@@ -13,8 +13,9 @@ namespace kernels
 
 		const size_t data_size{ data_length * general::word_size };
 
-		for (int i{ bx * bdim + tx }; i < data_size; i += gdim) {
-			char c{ data[i] };
+		char c;
+		for (size_t i{ bx * bdim + tx }; i < data_size; i += gdim) {
+			c = data[i];
 			if (c < 'Z' and c > 'A')
 				data[i] += 'a' - 'A';
 		}
@@ -24,7 +25,35 @@ namespace kernels
 		char* data
 		, const size_t data_length)
 	{
+		size_t tx{ threadIdx.x };
+		size_t bx{ blockIdx.x };
+		size_t bdim{ blockDim.x };
+		size_t gdim{ gridDim.x * blockDim.x };
 
+		size_t wdidx;
+		size_t j;
+		char c;
+		for (size_t i{ bx * bdim + tx }; i < data_length; i += gdim) {
+			// Word index
+			wdidx = i * general::word_size;
+
+			c = data[wdidx];
+			if (c < 'z' and c > 'a')
+				continue;
+
+			for (j = 1; j < general::word_size; ++j) {
+				c = data[wdidx + j];
+				if (c < 'z' and c > 'a') {
+					while (c < 'z' and c > 'a') {
+						data[wdidx] = c;
+						++wdidx;
+						c = data[wdidx + j];
+					}
+					data[wdidx] = '\0';
+					break;
+				}
+			}
+		}
 	}
 
     __global__ void countWords(
@@ -36,10 +65,10 @@ namespace kernels
 		__shared__ char kws[general::keywords_length * general::word_size];
 		__shared__ int lcl_hist[general::keywords_length];
 		
-		int tx{ threadIdx.x };
-		int bx{ blockIdx.x };
-		int bdim{ blockDim.x };
-		int gdim{ gridDim.x * blockDim.x };
+		size_t tx{ threadIdx.x };
+		size_t bx{ blockIdx.x };
+		size_t bdim{ blockDim.x };
+		size_t gdim{ gridDim.x * blockDim.x };
 
 		// Copy keywords to shared memory
 		size_t i{ tx };
@@ -54,21 +83,21 @@ namespace kernels
 		__syncthreads();
 
 		// Process data
+		size_t wdidx;
+		size_t j;
+		size_t kwidx;
+		bool equal;
+		size_t k;
 		for (i = bx * bdim + tx; i < data_length; i += gdim) {
 
 			// Word index
-			size_t wdidx;
 			wdidx = i * general::word_size;
 
 			// Search the keywords to find the index and update local histogram
-			size_t j;
 			for (j = 0; j < general::keywords_length; ++j) {
 				// Keyword index
-				size_t kwidx;
-				bool equal;
 				kwidx = j * general::word_size;
 				equal = true;
-				size_t k;
 				for (k = 0; k < general::word_size; ++k) {
 					if (kws[kwidx + k] == '\0' and data[wdidx + k] == '\0')
 						break;
