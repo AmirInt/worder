b604f995d88d8eccb6c2ea402b27b44ba133fb90
AmirInt <amir.intelli@gmail.com>
2023-06-30 12:01:03 +0100

    kernels.cuh: Define macro __CUDACC__ to make IDE recognise CUDA functions.

--- kernels.cuh
+++ kernels.cuh
@@ -1,6 +1,10 @@
 #ifndef WORDER_KERNELS_CUH_
 #define WORDER_KERNELS_CUH_
 
+#ifndef __CUDACC__
+#define __CUDACC__
+#endif
+
 // CUDA
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
